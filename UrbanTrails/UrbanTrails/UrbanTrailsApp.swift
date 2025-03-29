39ae167377f8ebfc43715d0febbccc88eb90aa30
RubbanIftikhar <iamrubban@gmail.com>
2025-03-29 12:10:38 +0500

    Set up the xcode project

---
+++ UrbanTrails/UrbanTrails/UrbanTrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanTrailsApp.swift
+//  UrbanTrails
+//
+//  Created by Rubban Iftikhar on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct UrbanTrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
